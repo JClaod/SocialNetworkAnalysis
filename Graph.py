c9d30eff2b473aa4438532e0a50bb56570769cf8
joshuaStefanLao <lao.joshuas@gmail.com>
2023-08-18 09:00:35 -0700

    Added graph class with Dijkstra Alg implementation

--- Graph.py
+++ Graph.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Graph:
     def __init__(self):
         self.id_to_names = []
@@ -10,18 +12,48 @@ class Graph:
         self.id_to_names.append([id, name])
         self.userKeys += 1
 
-    def addEdge(self, toNode, fromNode):
-        self.adjacencyList[toNode].append(fromNode)
-        self.adjacencyList[fromNode].append(toNode)
+    def addEdge(self, toID, fromID):
+        self.adjacencyList[toID].append(fromID)
+        self.adjacencyList[fromID].append(toID)
     
-    def localDegree(self, node):
-        return len(self.adjacencyList[node])
+    def localDegree(self, id):
+        return len(self.adjacencyList[id])
     
     def avgGlobalDegree(self):
         total_participants = len(self.adjacencyList)
         total_degree = 0
 
-        for item in self.adjacencyList:
-            total_degree += self.localDegree(item)
+        for id in self.adjacencyList:
+            total_degree += self.localDegree(id)
 
         return total_degree/total_participants
+    
+class GraphTraversal:
+    def __init__(self, g, source):
+        self.listOfPath = []
+        self.edgeTo = {id: float('inf') for id in self.g.adjacencyList}
+        self.distTo = {id: None for id in self.g.adjacencyList}
+        self.g = g
+        self.source = source
+    
+    def shortestPath(self):
+        self.distTo[self.source] = 0
+        queue = deque([self.source])
+
+        while queue:
+            current = queue.popleft()
+
+            for friend in self.g.adjacencyList[current]:
+                if self.distTo[friend] == float('inf'):
+                    self.distTo[friend] = self.distTo[current] + 1
+                    self.edgeTo[friend] = current
+                    queue.append(friend)
+
+    def pathToTarget(self, target):
+        listOfPath = []
+
+        while self.edgeTo(target) != None:
+            listOfPath.append(self.edgeTo[target])
+            target = self.edgeTo[target]
+        
+        print("Distance to target: " + str(len(listOfPath)) + ", Path to target: " + str(listOfPath))
